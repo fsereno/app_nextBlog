56396d61ac2585d755b52a83abaa2ed021bc0c00
Fabio Sereno <fabio_sereno@hotmail.com>
2021-09-14 21:20:11 +0100

    Looking at now using env vars

--- project/utils/connectionStrings.js
+++ project/utils/connectionStrings.js
@@ -1,1 +1,5 @@
-export const mongoDBConnectionString = "mongodb+srv://admin:QG8Dq0Xkwvw6PUNW@cluster0.smtwm.mongodb.net/blogs?retryWrites=true&w=majority";
+
+const user = process.env.MONGO_DB_USER;
+const password = process.env.MONGO_DB_PASSWORD;
+const database = process.env.MONGO_DB;
+export const mongoDBConnectionString = `mongodb+srv://${user}:${password}@cluster0.smtwm.mongodb.net/${database}?retryWrites=true&w=majority`;
