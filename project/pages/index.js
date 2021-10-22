a76bcbaeaafe7db9197f2252cdcca81313d9c3db
Fabio Sereno <fabio_sereno@hotmail.com>
2021-10-22 22:15:06 +0100

    Cont with styling

--- project/pages/index.js
+++ project/pages/index.js
@@ -21,14 +21,15 @@ export default function Home(props) {
       </div>
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.link}
+          href="https://fsereno.github.io/portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+           By{" "}
+          <strong className={styles.logo}>
+            Fabio Sereno
+          </strong>
         </a>
       </footer>
     </>
